e70306755db70bd2c92f96f65670a6057137c3a6
prakash19960202 <136340051+prakash19960202@users.noreply.github.com>
2023-07-05 16:58:56 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -11,3 +11,23 @@ provider "google" {
  credentials = "creds.json"
 
 }
+
+provider "google-beta" {
+
+ version = "~> 2.14"
+
+ project     = var.project
+
+ region      = var.region
+
+ zone        = var.zone
+
+ credentials = "creds.json"
+
+}
+
+provider "null" {
+
+ version = "~> 2.1"
+
+}
