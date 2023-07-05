05368e60f67c39959f821d83ce7e53fceab262f8
prakash19960202 <136340051+prakash19960202@users.noreply.github.com>
2023-07-05 17:05:02 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,28 +1,25 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "4.70.0"
+    }
+  }
+}
+
 provider "google" {
-
- version = "~> 2.14"
-
- project     = var.project
-
- region      = var.region
-
- zone        = var.zone
-
- credentials = "creds.json"
-
+  credentials=file("creds.json")
+  project = "groovy-karma-388506"
+  region  = "us-central1"
+  zone    = "us-central1-a"
 }
 
 provider "google-beta" {
 
- version = "~> 2.14"
-
- project     = var.project
-
- region      = var.region
-
- zone        = var.zone
-
- credentials = "creds.json"
+credentials=file("creds.json")
+  project = "groovy-karma-388506"
+  region  = "us-central1"
+  zone    = "us-central1-a"
 
 }
 
