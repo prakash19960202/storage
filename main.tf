087a3c30d6b0322ab51c17ecf917c5a6d225d64c
prakash19960202 <136340051+prakash19960202@users.noreply.github.com>
2023-07-05 16:32:58 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,13 @@
+variable "files" {
+  type = map(string)
+  default = {
+    # sourcefile = destfile
+    "folder/file1" = "C:/test/AdventureWorks2017.bak"
+  }
+}
+resource "google_storage_bucket_object" "my-config-objects" {
+  for_each = var.files
+  name     = each.value
+  source   = "${path.module}/${each.key}"
+  bucket   = sqlservermedia
+}
